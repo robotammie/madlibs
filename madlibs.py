d5abbfcfc168cf980514d439d40621adb341465a
Hackbright Student <info@hackbrightacademy.com>
2016-04-19 15:59:37 -0700

    added mad libs word entry form

--- madlibs.py
+++ madlibs.py
@@ -38,6 +38,19 @@ def greet_person():
                            person=player,
                            compliment=compliment)
 
+@app.route('/game')
+def show_game_form():
+    """docstring"""
+
+    play_game = request.args.get('play-game')
+
+    if play_game == "yes":
+        return render_template("game.html")
+
+    else:
+        return render_template('goodbye.html')
+
+
 
 if __name__ == '__main__':
     # debug=True gives us error messages in the browser and also "reloads" our web app
