67b7cb483654ae6bb9485dee55cf457819e7e21c
Hackbright Student <info@hackbrightacademy.com>
2016-04-20 12:44:17 -0700

    one form, random story

--- madlibs.py
+++ madlibs.py
@@ -11,6 +11,8 @@ AWESOMENESS = [
     'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza', 'oh-so-not-meh',
     'brilliant', 'ducky', 'coolio', 'incredible', 'wonderful', 'smashing', 'lovely']
 
+TEMPLATES = ['forest', 'sanfran']
+
 
 @app.route('/')
 def start_here():
@@ -57,32 +59,39 @@ def show_game_form():
 def show_madlib():
     """renders a delightfully hilarious story"""
 
+    # text inputs from form in play2.html
     adjective = request.form.get("adjective")
     adverb = request.form.get("adverb")
     body_part = request.form.get("body-part")
     noun = request.form.get("noun")
     plural_noun = request.form.get("plural-noun")
 
+    # dwarf list is a list of unicode strings (will print as normal strings)
     dwarf_input = request.form.getlist("dwarfs")
 
-    print dwarf_input
-    # all_dwarfs = ["Snow White"] + dwarf_input
+    # add Snow White to list of dwarfs
+    all_dwarfs = ["Snow White"] + dwarf_input
 
-    # if dwarf_input == []:
-    #     dwarfs = "Snow White"
-    #     dwarf = "Snow White"
-    # else: 
-    #     dwarfs = ", ".join(all_dwarfs[:-1]) + " and " + all_dwarfs[-1]
-    #     dwarf = choice(all_dwarfs)
+    # create string from list of dwarfs, and select one at random to have a starring role
+    if dwarf_input == []:
+        dwarfs = "Snow White"
+        dwarf = "Snow White"
+    else: 
+        dwarfs = ", ".join(all_dwarfs[:-1]) + " and " + all_dwarfs[-1]
+        dwarf = choice(all_dwarfs)
 
-    return render_template("madlibs2.html",
+    # choose which template to render
+    template = choice(TEMPLATES)
+
+    # feed variables into new template; let hilarity ensue
+    return render_template(template + ".html",
                            adjective=adjective,
                            adverb=adverb,
                            body=body_part,
                            noun=noun,
                            nouns=plural_noun,
-                           dwarfs=dwarf_input,
-                           dwarf=dwarf_input)
+                           dwarfs=dwarfs,
+                           dwarf=dwarf)
 
 
 
